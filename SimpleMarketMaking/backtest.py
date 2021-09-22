b12af530f9e7a12e00bb4c5610ef13d5483f0828
Tibor <tibor@auktechnology.com>
2021-09-22 19:15:42 +0100

    end of day commit - 22 September 2021.

--- SimpleMarketMaking/backtest.py
+++ SimpleMarketMaking/backtest.py
@@ -10,32 +10,43 @@ class Backtest:
     def __init__(self, data_frame: Optional[pd.DataFrame]) -> None:
         self.data_frame = data_frame
 
-        self.symbol = 'self.data_frame'
-        self.target_spread = 20
-        self.skew_at_20_pct = 0
-        self.skew_at_40_pct = 0
-        self.skew_at_max_pos = 0
-        self.max_pos = 100
-        self.target_pos = 20
-        self.mid_change_threshold = 10
+        self.target_spread = 30
+        self.max_pos = 200
+        self.target_position = 20
+        self.mid_change_threshold = 15
+        self.aggressive_brokerage = 0.0004
+        self.passive_brokerage = 0.0002
 
         self.position = 0
         self.cash = 0
-        self.bid = 0
-        self.ask = 0
-        self.mid = 0
-        self.last_mid = 0
-        self.pnl = 0
-        self.mid_changed = False
+        self.last_quoted_bid = 0
+        self.last_quoted_ask = 0
+        self.last_quoted_mid = 0
+        self.last_market_bid = 0
+        self.last_market_ask = 0
+        self.last_market_mid = 0
+        self.gross_pnl = 0
         self.skew = 0
+        self.fee = 0
+        self.net_pnl = 0
+        self.trade_side = 0
+        self.trade_size = 0
+        self.trade_price = 0
+        self.number_of_quote = 0
 
+        self.market_bids = []
+        self.market_asks = []
         self.market_mids = []
         self.skews = []
-        self.bids = []
-        self.asks = []
+        self.quoted_bids = []
+        self.quoted_asks = []
+        self.quoted_mids = []
         self.positions = []
         self.cashs = []
-        self.pnls = []
+        self.gross_pnls = []
+        self.net_pnls = []
+        self.number_of_quotes = []
+        self.fees = []
 
         self.trade_sides = []
         self.trade_sizes = []
@@ -45,6 +56,7 @@ class Backtest:
         self.hourly_timestamps = []
         self.hourly_pnls = []
         self.hourly_volumes = []
+        self.hourly_returns = []
 
     def run(self) -> None:
         n = len(self.data_frame)
@@ -53,90 +65,69 @@ class Backtest:
             if math.floor(100 * i / n) > progress_pct:
                 progress_pct = progress_pct + 10
                 print(str(progress_pct) + '% ', end='')
-            bid_price = self.data_frame['bidPrice'].iloc[i]
-            ask_price = self.data_frame['askPrice'].iloc[i]
+            self.last_market_bid = self.data_frame['bidPrice'].iloc[i]
+            self.last_market_ask = self.data_frame['askPrice'].iloc[i]
+            self.last_market_mid = 0.5 * (self.last_market_bid + self.last_market_ask)
+            if self.last_quoted_bid > 0 and self.last_quoted_bid >= self.last_market_ask:
+                self._execute_passive_buy(self.target_position)
+            if 0 < self.last_quoted_ask <= self.last_market_bid:
+                self._execute_passive_sell(self.target_position)
+            self.number_of_quote = 0
             if i < n - 1:
-                self.mid = 0.5 * (bid_price + ask_price)
-                self.market_mids.append(self.mid)
-                if self.bid > 0 and ask_price <= self.bid:
-                    self.position = self.position + self.target_pos
-                    self.cash = self.cash - self.target_pos * self.bid
-                    self.trade_sides.append(1)
-                    self.trade_sizes.append(self.target_pos)
-                    self.trade_prices.append(self.bid)
-                    self.bid = 0
-                elif 0 < self.ask <= bid_price:
-                    self.position = self.position - self.target_pos
-                    self.cash = self.cash + self.target_pos * self.ask
-                    self.trade_sides.append(-1)
-                    self.trade_sizes.append(self.target_pos)
-                    self.trade_prices.append(self.ask)
-                    self.ask = 0
-                else:
-                    self.trade_sides.append(0)
-                    self.trade_sizes.append(0)
-                    self.trade_prices.append(0)
-                self.positions.append(self.position)
-                self.mid_changed = abs(self.mid - self.last_mid) >= self.mid_change_threshold
-                self.last_mid = self.mid
-                self.skew = 0
-                if 0.2 * self.max_pos <= self.position < 0.4 * self.max_pos:
-                    self.skew = -self.skew_at_20_pct
-                elif 0.4 * self.max_pos <= self.position < self.max_pos:
-                    self.skew = -self.skew_at_40_pct
-                elif self.position >= self.max_pos:
-                    self.skew = -self.skew_at_max_pos
-                if -0.4 * self.max_pos < self.position <= -0.2 * self.max_pos:
-                    self.skew = self.skew_at_20_pct
-                elif -self.max_pos < self.position <= -0.4 * self.max_pos:
-                    self.skew = self.skew_at_40_pct
-                elif self.position <= -self.max_pos:
-                    self.skew = self.skew_at_max_pos
-                self.skews.append(self.skew)
-                if self.mid_changed or self.bid == 0 or self.ask == 0:
-                    self.bid = math.ceil(self.mid + self.skew - 0.5 * self.target_spread)
-                    self.ask = math.floor(self.mid + self.skew + 0.5 * self.target_spread)
+                self.compute_skew()
+                if self.last_quoted_bid == 0 or self.last_quoted_ask == 0 or \
+                        np.abs(self.last_market_mid - self.last_quoted_mid) >= self.mid_change_threshold:
+                    self.last_quoted_bid = math.ceil(self.last_market_mid + self.skew - 0.5 * self.target_spread)
+                    self.last_quoted_ask = math.floor(self.last_market_mid + self.skew + 0.5 * self.target_spread)
+                    self.last_quoted_mid = 0.5 * (self.last_quoted_bid + self.last_quoted_ask)
+                    self.number_of_quote = 2
+                if self.last_quoted_bid > 0 and self.last_quoted_bid >= self.last_market_ask:
+                    self._execute_aggressive_buy(self.target_position)
+                if 0 < self.last_quoted_ask <= self.last_market_bid:
+                    self._execute_aggressive_sell(self.target_position)
             else:
-                self.market_mids.append(self.last_mid)
-                self.skews.append(0)
                 if self.position > 0:
-                    self.cash = self.cash + self.position * bid_price
-                    self.trade_sides.append(-1)
-                    self.trade_sizes.append(self.position)
-                    self.trade_prices.append(bid_price)
-                elif self.position < 0:
-                    self.cash = self.cash - self.position * ask_price
-                    self.trade_sides.append(1)
-                    self.trade_sizes.append(-self.position)
-                    self.trade_prices.append(ask_price)
-                else:
-                    self.trade_sides.append(0)
-                    self.trade_sizes.append(0)
-                    self.trade_prices.append(0)
-                self.position = 0
-                self.positions.append(self.position)
-            self.bids.append(self.bid)
-            self.asks.append(self.ask)
-            self.pnl = self.cash + self.position * self.mid
-            self.pnls.append(self.pnl)
-            self.cashs.append(self.cash)
-        self.data_frame['market_mid'] = self.market_mids
-        self.data_frame['bid'] = self.bids
-        self.data_frame['ask'] = self.asks
-        self.data_frame['position'] = self.positions
-        self.data_frame['cash'] = self.cashs
+                    self._execute_aggressive_sell(self.position)
+                if self.position < 0:
+                    self._execute_aggressive_buy(-self.position)
+            self.gross_pnl = self.cash + self.position * self.last_market_mid
+            self.net_pnl = self.gross_pnl - self.fee
+            self.market_bids.append(self.last_market_bid)
+            self.market_asks.append(self.last_market_ask)
+            self.market_mids.append(self.last_market_mid)
+            self.quoted_bids.append(self.last_quoted_bid)
+            self.quoted_asks.append(self.last_quoted_ask)
+            self.quoted_mids.append(self.last_quoted_mid)
+            self.skews.append(self.skew)
+            self.positions.append(self.position)
+            self.gross_pnls.append(self.gross_pnl)
+            self.fees.append(self.fee)
+            self.net_pnls.append(self.net_pnl)
+            self.trade_sides.append(self.trade_side)
+            self.trade_sizes.append(self.trade_size)
+            self.trade_prices.append(self.trade_price)
+            self.number_of_quotes.append(self.number_of_quote)
+        self.data_frame['market_bid'] = list(map(lambda x: x / 10000, self.market_bids))
+        self.data_frame['market_ask'] = list(map(lambda x: x / 10000, self.market_asks))
+        self.data_frame['market_mid'] = list(map(lambda x: x / 10000, self.market_mids))
+        self.data_frame['quoted_bid'] = list(map(lambda x: x / 10000, self.quoted_bids))
+        self.data_frame['quoted_ask'] = list(map(lambda x: x / 10000, self.quoted_asks))
+        self.data_frame['quoted_mid'] = list(map(lambda x: x / 10000, self.quoted_mids))
         self.data_frame['skew'] = self.skews
-        self.data_frame['pnl'] = self.pnls
+        self.data_frame['position'] = self.positions
         self.data_frame['trade_side'] = self.trade_sides
         self.data_frame['trade_size'] = self.trade_sizes
-        self.data_frame['trade_price'] = self.trade_prices
+        self.data_frame['trade_price'] = list(map(lambda x: x / 10000, self.trade_prices))
+        self.data_frame['gross_pnl'] = list(map(lambda x: x / 10000, self.gross_pnls))
+        self.data_frame['fee'] = list(map(lambda x: x / 10000, self.fees))
+        self.data_frame['net_pnl'] = list(map(lambda x: x / 10000, self.net_pnls))
 
     def get_total_volume_traded(self) -> int:
         volume = sum(self.trade_sizes)
         return volume
 
     def get_yield(self) -> float:
-        y = 1e2 * self.pnl / self.get_total_volume_traded()
+        y = 1e2 * self.gross_pnl / self.get_total_volume_traded()
         return y
 
     def load_market_data(self, directory: str, start_date: datetime.date, number_of_days: int) -> None:
@@ -149,17 +140,27 @@ class Backtest:
             market_data = market_data.append(data_frame)
         self.data_frame = market_data
 
-    def get_average_hourly_return(self) -> float:
-        if len(self.hourly_returns) == 0:
-            self.generate_hourly_returns()
-        average_hourly_returns = float(np.average(self.hourly_returns))
-        return average_hourly_returns
+    def get_maximum_drawdown(self) -> float:
+        maximum_drawdown = 0
+        maximum_pnl = 0
+        minimum_pnl = 0
+        for pnl in self.gross_pnls:
+            if pnl > maximum_pnl:
+                maximum_pnl = pnl
+                minimum_pnl = pnl
+            if pnl < minimum_pnl:
+                minimum_pnl = pnl
+                if (maximum_pnl - minimum_pnl) > maximum_drawdown:
+                    maximum_drawdown = maximum_pnl - minimum_pnl
+        return maximum_drawdown
 
-    def generate_hourly_indices(self) -> None:
+    def generate_hourly_indices_and_timestamps(self) -> None:
         i = 0
         timestamp = self.data_frame['millisecondsSinceEpoch'].iloc[0]
         timestamp = timestamp - (timestamp % 3600000) + 3600000
         n = len(self.data_frame.index)
+        self.hourly_indices = []
+        self.hourly_timestamps = []
         while i < n:
             while i < n and self.data_frame['millisecondsSinceEpoch'].iloc[i] < timestamp:
                 i = i + 1
@@ -167,47 +168,52 @@ class Backtest:
             self.hourly_timestamps.append(timestamp)
             timestamp = timestamp + 3600000
 
-    def get_standard_deviation_of_hourly_returns(self) -> float:
-        if len(self.hourly_returns) == 0:
-            self.generate_hourly_returns()
-        standard_deviation_of_hourly_returns = float(np.std(self.hourly_returns))
-        return standard_deviation_of_hourly_returns
+    def generate_hourly_pnls(self) -> None:
+        if not self.hourly_indices:
+            self.generate_hourly_indices_and_timestamps()
+        self.hourly_pnls = []
+        for i in range(len(self.hourly_indices) - 1):
+            self.hourly_pnls.append(self.gross_pnls[self.hourly_indices[i + 1]] - self.gross_pnls[self.hourly_indices[i]])
 
-    def get_annualised_average_hourly_return(self) -> float:
-        annualised_average_hourly_return = 24 * 365.24 * self.get_average_hourly_return()
-        return annualised_average_hourly_return
-
-    def get_annualised_standard_deviation_of_hourly_returns(self) -> float:
-        annualised_standard_deviation_of_hourly_returns = np.sqrt(
-            24 * 365.24) * self.get_standard_deviation_of_hourly_returns()
-        return annualised_standard_deviation_of_hourly_returns
-
-    def get_annualised_sharp_ratio(self) -> float:
-        sharp_ratio = self.get_annualised_average_hourly_return() / \
-                      self.get_annualised_standard_deviation_of_hourly_returns()
-        return sharp_ratio
-
-    def get_maximum_drawdown(self) -> float:
-        maximum_drawdown = 0
-        maximum_pnl = 0
-        minimum_pnl = 0
-        for pnl in self.pnls:
-            if pnl > maximum_pnl:
-                maximum_pnl = pnl
-                if maximum_pnl - minimum_pnl > maximum_drawdown:
-                    maximum_drawdown = (maximum_pnl - minimum_pnl)/maximum_pnl
-            if pnl < minimum_pnl:
-                minimum_pnl = pnl
-        return maximum_drawdown
+    def generate_hourly_volumes(self) -> None:
+        if not self.hourly_indices:
+            self.generate_hourly_indices_and_timestamps()
+        cumulative_volume = 0
+        cumulative_volumes = []
+        for i in range(len(self.trade_sizes)):
+            cumulative_volume = cumulative_volume + self.trade_sizes[i] * self.trade_prices[i]
+            cumulative_volumes.append(cumulative_volume)
+        self.hourly_volumes = []
+        for i in range(len(self.hourly_indices) - 1):
+            self.hourly_volumes.append(cumulative_volumes[self.hourly_indices[i + 1]] -
+                                       cumulative_volumes[self.hourly_indices[i]])
 
     def generate_hourly_returns(self) -> None:
-        if len(self.hourly_indices) == 0:
-            self.generate_hourly_indices()
-        n = len(self.hourly_indices)
-        for i in range(n - 1):
-            self.hourly_returns.append((self.pnls[self.hourly_indices[i + 1]] -
-                                        self.pnls[self.hourly_indices[i]]) /
-                                       float(np.abs(self.pnls[self.hourly_indices[i]])))
+        if not self.hourly_pnls:
+            self.generate_hourly_pnls()
+        if not self.hourly_volumes:
+            self.generate_hourly_volumes()
+        self.hourly_returns = []
+        for i in range(len(self.hourly_pnls)):
+            if self.hourly_volumes[i] > 0:
+                self.hourly_returns.append(self.hourly_pnls[i] / self.hourly_volumes[i])
+            else:
+                self.hourly_returns.append(np.nan)
+
+    def get_annualised_return(self) -> float:
+        if not self.hourly_returns:
+            self.generate_hourly_returns()
+        mean_hourly_return = np.nanmean(self.hourly_returns)
+        annualised_return = 24 * 365.24 * mean_hourly_return
+        return annualised_return
+
+    def get_annualised_sharp_ratio(self) -> float:
+        if not self.hourly_returns:
+            self.generate_hourly_returns()
+        annualised_return = self.get_annualised_return()
+        annualised_stddev = self.get_annualised_standard_deviation()
+        annualised_sharp = annualised_return / annualised_stddev
+        return annualised_sharp
 
     def get_number_of_trades(self) -> int:
         number_of_trades = 0
@@ -218,3 +224,55 @@ class Backtest:
     def generate_trades(self) -> pd.DataFrame:
         trades = self.data_frame[self.data_frame['trade_side'] != 0]
         return trades
+
+    def get_final_pnl(self) -> float:
+        if not self.hourly_indices:
+            self.generate_hourly_indices_and_timestamps()
+        pnl = self.gross_pnls[self.hourly_indices[-1]]
+        return pnl
+
+    def get_annualised_standard_deviation(self) -> float:
+        if not self.hourly_returns:
+            self.generate_hourly_returns()
+        stddev_hourly_return = np.nanstd(self.hourly_returns)
+        annualised_stddev = 24 * 365.24 * stddev_hourly_return
+        return annualised_stddev
+
+    def compute_skew(self) -> None:
+        if np.abs(self.position) >= self.max_pos:
+            self.skew = int(-2 * np.sign(self.position) * self.target_spread)
+        else:
+            self.skew = int(-1 * np.sign(self.position) * np.floor(self.target_spread * np.abs(self.position) /
+                                                                   self.max_pos))
+
+    def _execute_aggressive_buy(self, size: int) -> None:
+        self.fee = size * self.last_market_ask * self.aggressive_brokerage
+        self._execute_buy(size)
+
+    def _execute_buy(self, size: int) -> None:
+        self.last_quoted_bid = 0
+        self.position = self.position + size
+        self.cash = self.cash - size * self.last_market_ask
+        self.trade_side = 1
+        self.trade_size = size
+        self.trade_price = self.last_market_ask
+
+    def _execute_aggressive_sell(self, size: int) -> None:
+        self.fee = size * self.last_market_bid * self.aggressive_brokerage
+        self._execute_sell(size)
+
+    def _execute_sell(self, size: int) -> None:
+        self.last_quoted_ask = 0
+        self.position = self.position - size
+        self.cash = self.cash + size * self.last_market_bid
+        self.trade_side = -1
+        self.trade_size = size
+        self.trade_price = self.last_market_bid
+
+    def _execute_passive_buy(self, size: int) -> None:
+        self.fee = size * self.last_quoted_bid * self.passive_brokerage
+        self._execute_buy(size)
+
+    def _execute_passive_sell(self, size: int) -> None:
+        self.fee = size * self.last_quoted_ask * self.passive_brokerage
+        self._execute_sell(size)
--- SimpleMarketMaking/main.py
+++ SimpleMarketMaking/main.py
@@ -26,33 +26,45 @@ start_time = datetime.datetime.now()
 #     date = date + datetime.timedelta(days=d)
 #     write_data_to_file(date)
 
-for target_spread in [100]:
+for target_spread in [30]:
     my_backtest = backtest.Backtest(None)
-    my_backtest.load_market_data(directory, datetime.date(2021, 9, 8), 9)
+    my_backtest.load_market_data(directory, datetime.date(2021, 9, 8), 1)
     my_backtest.target_spread = target_spread
-    my_backtest.skew_at_max_pos = 10*target_spread
     my_backtest.run()
-    pnl = my_backtest.pnl / 10000
-    volume = my_backtest.get_total_volume_traded()
     number_of_trades = my_backtest.get_number_of_trades()
-    y = my_backtest.get_yield()
-    annualised_return = my_backtest.get_annualised_average_hourly_return()
-    annualised_sharp_ratio = my_backtest.get_annualised_sharp_ratio()
-    maximum_drawdown = my_backtest.get_maximum_drawdown()
-    print('')
-    print('target_spread = ' + str(target_spread) +
-          ' pips\t: pnl = ' + str(pnl) +
-          ' USD\t, number of trades = ' + str(number_of_trades) +
-          '\t, volume = ' + str(volume) +
-          '\t, annualised return = ' + str("{:.2f}".format(100*annualised_return)) +
-          '%\t, sharp = ' + str("{:.2f}".format(annualised_sharp_ratio)) +
-          '\t, MDD = ' + str("{:.2f}".format(100*maximum_drawdown)) +
-          '%\t, yield = ' + str("{:.2f}".format(y)) +
-          ' $/M$.')
-    my_backtest_sampled = my_backtest.data_frame.iloc[::math.ceil(len(my_backtest.data_frame.index)/100000), :]
-    my_backtest_sampled.to_csv(directory + 'results_backtest_pnls_' + str(target_spread) + '.csv')
-    trades = my_backtest.generate_trades()
-    trades.to_csv(directory + 'results_backtest_trades_' + str(target_spread) + '.csv')
+    if number_of_trades >= 100:
+        pnl = my_backtest.get_final_pnl()
+        volume = my_backtest.get_total_volume_traded()
+        total_yield = my_backtest.get_yield()
+        annualised_return = my_backtest.get_annualised_return()
+        annualised_standard_deviation = my_backtest.get_annualised_standard_deviation()
+        annualised_sharp_ratio = my_backtest.get_annualised_sharp_ratio()
+        maximum_drawdown = my_backtest.get_maximum_drawdown()
+        number_of_quotes = sum(my_backtest.number_of_quotes)
+        fees = sum(my_backtest.fees)
+        print('')
+        print('target_spread = ' + str(target_spread) +
+              ' pips\t: pnl = ' + str("{:,.2f}".format(pnl/10000)) +
+              ' USD,\t fees = ' + str("{:,.2f}".format(fees/10000)) +
+              ' USD,\t net pnl = ' + str("{:,.2f}".format((pnl - fees)/10000)) +
+              ' USD,\t number of quotes = ' + str("{:,.0f}".format(number_of_quotes)) +
+              ',\t number of trades = ' + str("{:,.0f}".format(number_of_trades)) +
+              ',\t volume = ' + str("{:,.0f}".format(volume)) +
+              ' USD,\t annualised return = ' + str("{:,.2f}".format(100*annualised_return)) +
+              '%,\t annualised standard deviation of returns = ' +
+              str("{:,.2f}".format(100*annualised_standard_deviation)) +
+              '%,\t annualised sharp = ' + str("{:,.2f}".format(annualised_sharp_ratio)) +
+              ',\t MDD = ' + str("{:,.0f}".format(maximum_drawdown)) +
+              ' USD,\t total yield = ' + str("{:,.2f}".format(total_yield)) +
+              ' $/M$.')
+        # my_backtest_sampled = my_backtest.data_frame.iloc[::math.ceil(len(my_backtest.data_frame.index)/100000), :]
+        my_backtest_sampled = my_backtest.data_frame
+        my_backtest_sampled.to_csv(directory + 'results_backtest_pnls_' + str(target_spread) + '.csv')
+        trades = my_backtest.generate_trades()
+        trades.to_csv(directory + 'results_backtest_trades_' + str(target_spread) + '.csv')
+    else:
+        print()
+        print('target_spread = ' + str(target_spread) + ': less than 100 trades')
 
 end_time = datetime.datetime.now()
 print('--- ran in ' + str(end_time - start_time))
