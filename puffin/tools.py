60a959de5c96e5548c7a5c4f5b8c1297028e27c6
Tibor <tibor@auktechnology.com>
2021-12-06 11:54:04 +0000

    adding note books

--- puffin/tools.py
+++ puffin/tools.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 import puffin.config as con
+import boto3
+import os
 
 
 def get_id_from_symbol(symbol: str) -> int:
@@ -225,3 +227,46 @@ def get_dollar_trade_side_runs_sampled_indices(trades_formatted: pd.DataFrame) -
     b = b.multiply(trades_formatted['price'])
     data_frame = _get_runs_sampled_indices(b)
     return data_frame
+
+
+def s3_download_file(local_filename, bucket_name, remote_filename):
+    print("DOWNLOAD FILE FROM S3: s3://" + bucket_name + '/' + remote_filename + ' -> ' + local_filename)
+    s3_cl = boto3.client('s3', endpoint_url=os.getenv('BOTO3_S3_ENDPOINT_URL'))
+    with open(local_filename, 'wb') as f:
+        s3_cl.download_fileobj(bucket_name, remote_filename, f)
+
+
+def s3_upload_file(local_filename, bucket_name, remote_filename):
+    print("UPLOAD FILE TO S3: " + local_filename + ' -> ' + 's3://' + bucket_name + '/' + remote_filename)
+    s3_cl = boto3.client('s3', endpoint_url=os.getenv('BOTO3_S3_ENDPOINT_URL'))
+    with open(local_filename, 'rb') as f:
+        s3_cl.upload_fileobj(f, bucket_name, remote_filename)
+
+
+def s3_list(bucket_name, prefix_path):
+    print("LIST FROM S3: s3://" + bucket_name + "/" + prefix_path)
+    s3_cl = boto3.client('s3', endpoint_url=os.getenv('BOTO3_S3_ENDPOINT_URL'))
+    keys = []
+    # paginated shite
+    paginator = s3_cl.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=bucket_name, Prefix=prefix_path)
+    for page in pages:
+        for obj in page['Contents']:
+            keys.append({'name': obj['Key'], 'size': obj['Size']})
+    return keys
+
+
+def s3_download_directory(local_directory, bucket_name, remote_directory):
+    print("DOWNLOAD DIRECTORY FROM S3: s3://" + bucket_name + '/' + remote_directory + ' -> ' + local_directory)
+    remote_files = s3_list(bucket_name, remote_directory)
+    prefix = remote_directory + '/'
+    for rf in remote_files:
+        remote_file = rf['name']
+        size = rf['size']
+        if size == 0:
+            continue
+        stripped_path = remote_file[len(prefix):]
+        local_file = os.path.join(local_directory, stripped_path)
+        local_file_dir = os.path.dirname(local_file)
+        os.makedirs(local_file_dir, exist_ok=True)
+        s3_download_file(local_file, bucket_name, remote_file)
