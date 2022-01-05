e6c9203597b3ccda11a1c22e679dc9674974f781
daviidarr <davidrelkin@gmail.com>
2022-01-05 04:48:35 +0000

    bug fix

--- DerivativeArbitrage/ftx_orders.py
+++ DerivativeArbitrage/ftx_orders.py
@@ -9,16 +9,17 @@ from ftx_utilities import *
 placement_latency = 0.1
 amend_speed = 1
 amend_tolerance = 2
+taker_trigger = 0.002
 
 def place_postOnly(exchange: Exchange,
                    ticker: str,
                    side: str,
-                   size: float# in USD
+                   size: float# in coin
                     ) -> dict:
     status = 'new'
     while status != 'open':
-        top_of_book = exchange.fetch_ticker(ticker)['bid' if side == 'buy' else 'ask']*1.001
-        order = exchange.createOrder(ticker, 'limit', side, size/top_of_book, price=top_of_book,
+        top_of_book = exchange.fetch_ticker(ticker)['bid' if side == 'buy' else 'ask']
+        order = exchange.createOrder(ticker, 'limit', side, size, price=top_of_book,
                                      params={'postOnly': True})
         sleep(placement_latency)
         order_status = exchange.fetch_order(order['id'])
@@ -39,7 +40,7 @@ def chase(exchange: Exchange,
             order = exchange.createOrder(order_status['symbol'], 'market', order_status['side'], order_status['remaining']/trigger_level) ## TODO: what if fail ?
             return order
 
-        since = datetime.now().timestamp() * 1000 - 10*amend_speed * 1000
+        since = datetime.now().timestamp() * 1000 - 100*amend_speed * 1000
         history = pd.DataFrame(exchange.fetch_trades(order_status['symbol'], since=since))
         volatility = history['price'].diff().std() # TODO: yuky
 
@@ -59,8 +60,8 @@ def chase(exchange: Exchange,
 
 def execute_spread(exchange: Exchange, buy_ticker: str, sell_ticker: str, size: int) -> None:
     ## get 5m histories
-    nowtime = datetime.now().timestamp()
-    since = nowtime*1000-5*60*1000
+    nowtime = datetime.now().timestamp()*1000
+    since = nowtime-5*60*1000
     buy_history=pd.DataFrame(exchange.fetch_trades(buy_ticker,since=since)).set_index('timestamp')
     sell_history = pd.DataFrame(exchange.fetch_trades(sell_ticker, since=since)).set_index('timestamp')
 
@@ -69,16 +70,23 @@ def execute_spread(exchange: Exchange, buy_ticker: str, sell_ticker: str, size: 
     sell_volume = sell_history['amount'].sum()
     (first_ticker,second_ticker,first_side,second_side) = (buy_ticker,sell_ticker,'buy','sell') if buy_volume<sell_volume else (sell_ticker,buy_ticker,'sell','buy')
 
-    order = place_postOnly(exchange, first_ticker, first_side, size)
-    order = chase(exchange,order,999)
-    order = place_postOnly(exchange, second_ticker, second_side, size)
-    order = chase(exchange,order)
+    try:
+        order = place_postOnly(exchange, first_ticker, first_side, size)
+        order = chase(exchange,order,taker_trigger=999) # no trigger for first leg
+        order = place_postOnly(exchange, second_ticker, second_side, size)
+        order = chase(exchange,order,taker_trigger=taker_trigger)
 
-    return exchange.fetch_orders(buy_ticker,nowtime),exchange.fetch_orders(sell_ticker,nowtime)
+    # TODO: also cancel pre-existing ones...
+    exchange.cancel_all_orders(symbol=buy_ticker)
+    exchange.cancel_all_orders(symbol=sell_ticker)
+
+    buy_recap = pd.DataFrame(exchange.fetch_orders(buy_ticker,nowtime))
+    sell_recap = pd.DataFrame(exchange.fetch_orders(sell_ticker, nowtime))
+    return buy_recap.append(sell_recap)
 
 if True:
     exchange = open_exchange('ftx')
     buy_ticker = 'ETH-PERP'
     sell_ticker = 'ETH/USD'
-    size = 10
+    size = 10/exchange.fetch_ticker(sell_ticker)['close']
     execute_spread(exchange, buy_ticker, sell_ticker, size)
