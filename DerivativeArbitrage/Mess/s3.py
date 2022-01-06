848d4ec1b7c70d0f626eb5fc1d04396d48ad80b8
daviidarr <davidrelkin@gmail.com>
2022-01-06 18:07:13 +0000

    minor change

--- DerivativeArbitrage/Mess/s3.py
+++ DerivativeArbitrage/Mess/s3.py
@@ -1,5 +1,23 @@
 import boto3, os
 
+# https://betterprogramming.pub/cron-job-patterns-in-aws-126fbf54a276
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2')
+    ec2.request_spot_instances(
+        SpotPrice='<max price for spot instance, ex. 0.1>',
+        InstanceCount=1,
+        Type='one-time',
+        LaunchSpecification={
+            'ImageId': '<ami id>',
+            'KeyName': '<keypair to use>',
+            'InstanceType': '<instance type, ex. "t2.micro">',
+            'UserData': '<base64 encoded script goes here>',
+            'IamInstanceProfile': {
+                'Arn': '<instance role arn>' #make sure this role has terminate EC2 instance access
+            },
+            'SecurityGroupIds': ['<list of security group ids>']
+        }
+ )
 
 def s3_download_file(local_filename, bucket_name, remote_filename):
     print("DOWNLOAD FILE FROM S3: s3://" + bucket_name + '/' + remote_filename + ' -> ' + local_filename)
