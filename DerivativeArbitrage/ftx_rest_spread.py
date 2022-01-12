e4de3fe7755efc3f66261c38179b2772fbcbafbc
daviidarr <davidrelkin@gmail.com>
2022-01-12 00:31:34 +0000

    cleanup + tweak chase

--- DerivativeArbitrage/ftx_rest_spread.py
+++ DerivativeArbitrage/ftx_rest_spread.py
@@ -1,3 +1,4 @@
+import copy
 from time import sleep,perf_counter
 import asyncio
 import dateutil.parser
@@ -11,10 +12,12 @@ from ftx_portfolio import diff_portoflio
 
 placement_latency = 0.1# in sec
 amend_speed = 10.# in sec
-amend_trigger = 0.0005
+amend_trigger = 0.
 taker_trigger = 0.002
 slice_factor = 10.# in USD
 audit = []
+
+#################### various helpers ##############################
 
 def timer(func):
     """Print the runtime of the decorated function"""
@@ -29,6 +32,49 @@ def timer(func):
         #audit += [{'audit_type': 'timer', 'func': func.__name__, 'start_time': start_time, 'run_time': run_time}]
         return value
     return wrapper_timer
+
+class PlacementStrategy:
+    def __init__(self,exchange: Exchange,
+                              ticker: str,
+                              side: str,
+                              size: float  # in coin
+                              ):
+        self._ticker = ticker
+        self._side = side
+        self._size = size
+    def update(self)->None:
+        return None
+
+class AggressivePost:
+    def __init__(self, exchange: Exchange,
+                 ticker: str,
+                 side: str,
+                 size: float,  # in coin
+                 increments_from_opposite_tob: int):
+        self.__init__(exchange, ticker, side, size)
+        self._increments_from_opposite_tob = increments_from_opposite_tob
+    def update(self)->None:
+        return None
+
+def most_illiquid(exchange,ticker_buy,ticker_sell):
+    ## get 5m histories
+    nowtime = datetime.now().timestamp()
+    since = nowtime - 5 * 60
+    try:
+        history1 = pd.DataFrame(exchange.fetch_trades(ticker_buy, since=since*1000)).set_index('timestamp')
+        history2 = pd.DataFrame(exchange.fetch_trades(ticker_sell, since=since*1000)).set_index('timestamp')
+    except Exception as e:
+        print(f"bad history for either {ticker_buy} or {ticker_sell}. Passing {ticker_sell}")
+        return ticker_sell
+
+    ## least active first
+    volume1 = history1['amount'].sum()
+    volume2 = history2['amount'].sum()
+
+    result = ticker_buy if volume1<volume2 else ticker_sell
+    return result
+
+#################### low level orders, with checks and resend ##############################
 
 @timer
 async def sure_cancel(exchange: Exchange,order: str) -> dict:
@@ -60,48 +106,32 @@ async def monitored_market_order(exchange: Exchange,
 
     return mkt_order
 
-class PlacementStrategy:
-    def __init__(self,exchange: Exchange,
-                              ticker: str,
-                              side: str,
-                              size: float  # in coin
-                              ):
-        self._ticker = ticker
-        self._side = side
-        self._size = size
-    def update(self)->None:
-        return None
-
-class AggressivePost:
-    def __init__(self, exchange: Exchange,
-                 ticker: str,
-                 side: str,
-                 size: float,  # in coin
-                 increments_from_opposite_tob: int):
-        self.__init__(exchange, ticker, side, size)
-        self._increments_from_opposite_tob = increments_from_opposite_tob
-    def update(self)->None:
-        return None
 # ensures postOnly
 # places limit right before top of book
 # if canceled (because through mid, typically), try farther and farther to fight momentum
 @timer
-async def sure_postAggressive(exchange: Exchange,
+async def sure_post(exchange: Exchange,
                               ticker: str,
                               side: str,
-                              size: float  # in coin
-                              ) -> dict:
+                              size: float,  # in coin
+                              px_increment: float,
+                              mode: str = 'passive') -> dict:# shift pushes towards aggressive
     sizeIncrement=float(exchange.fetch_ticker(ticker)['info']['sizeIncrement'])
-    if (size<sizeIncrement):
-        print(f'skip {size} < {sizeIncrement}')
-        return dict()
-    px_increment = float(exchange.fetch_ticker(ticker)['info']['priceIncrement'])
-    attempt = 0 # start one increment in front of tob
+    if (size>sizeIncrement/2)&(size<sizeIncrement):
+        size = sizeIncrement
+        print(f'rounded up {ticker} in {size} to {sizeIncrement}')
+
+    attempt = 1 # start one increment in front of tob
     status = 'canceled'
     while status == 'canceled':
-        top_of_book = exchange.fetch_ticker(ticker)['bid' if side == 'buy' else 'ask']
         # try farther and farther to fight momentum
-        limit_price = top_of_book - (1 if side=='buy' else -1)*(attempt-1)*px_increment
+        if mode=='aggressive':
+            top_of_book = exchange.fetch_ticker(ticker)['ask' if side == 'buy' else 'bid']
+            limit_price = top_of_book - (1 if side == 'buy' else -1) * attempt * px_increment
+        elif mode=='passive':
+            top_of_book = exchange.fetch_ticker(ticker)['bid' if side == 'buy' else 'ask']
+            limit_price = top_of_book + (1 if side == 'buy' else -1) * (2-attempt) * px_increment
+
         order = exchange.createOrder(ticker, 'limit', side, size,
                                      price=limit_price,
                                      params={'postOnly': True})
@@ -120,91 +150,45 @@ async def sure_postAggressive(exchange: Exchange,
         attempt+=1
     return order
 
+#################### complex order: post_chase_trigger ##############################
+
 # doesn't stop on partial fills
-async def chase(exchange: Exchange,order: str, taker_trigger: float) -> dict:
+# size in coin
+async def post_chase_trigger(exchange: Exchange,
+                            ticker: str, size: float,
+                            taker_trigger: float) -> dict:
+    # post
+    fetched = exchange.fetch_ticker(ticker)['info']
+    increment = float(fetched['priceIncrement'])
+    initial_order = await sure_post(exchange,ticker,'buy' if size>0 else 'sell',np.abs(size),increment, mode='passive')
+    trigger_level = initial_order['price'] * (1 + taker_trigger * (1 if size >0 else -1))
+
+    await asyncio.sleep(amend_speed)
+    order = initial_order
     order_status = exchange.fetch_order(order['id'])
-    ticker=order_status['symbol']
-    side=order_status['side']
-    trigger_level = order['price'] * (1 + taker_trigger * (1 if order['side'] == 'buy' else -1))
-    attempt=0
     while order_status['status'] == 'open':
-        top_of_book = exchange.fetch_ticker(ticker)['bid' if side=='buy' else 'ask']
+        side = order['side']
+        opposite_tob = exchange.fetch_ticker(ticker)['ask' if side=='buy' else 'bid']
 
         # if loss too big vs initial level, stop out
-        if (1 if side=='buy' else -1)*(top_of_book-trigger_level)>0:
+        if (1 if side=='buy' else -1)*(opposite_tob-trigger_level)>0:
             await sure_cancel(exchange, order)
             stop_order = await monitored_market_order(exchange,ticker, side, order_status['remaining']) ## TODO: what if fail ?
             return stop_order
 
-        # if move is material, chase
-
-        # calibrate amend threshold
-        #end_time = datetime.now().timestamp() * 1000
-        #start_time = end_time - 100 * amend_speed * 1000
-        #volatility = underlying_vol(exchange,symbol, start_time, end_time)
-
-        if np.abs(top_of_book/order_status['price']-1)>amend_trigger:
+        # if move is material, chase more and more aggressively
+        if np.abs(opposite_tob/order_status['price']-1)>amend_trigger:
             # ftx modify order isn't a good deal. cancel.
+            increment*=2
             await sure_cancel(exchange,order)
-            order = await sure_postAggressive(exchange, ticker, side, order_status['remaining'])
+            order = await sure_post(exchange, ticker, side, order_status['remaining'],increment,mode='passive')
 
         await asyncio.sleep(amend_speed)
         order_status = exchange.fetch_order(order['id'])
 
     return order
 
-def most_illiquid(exchange,ticker_buy,ticker_sell):
-    ## get 5m histories
-    nowtime = datetime.now().timestamp() * 1000
-    since = nowtime - 5 * 60 * 1000
-    try:
-        history1 = pd.DataFrame(exchange.fetch_trades(ticker_buy, since=since)).set_index('timestamp')
-        history2 = pd.DataFrame(exchange.fetch_trades(ticker_sell, since=since)).set_index('timestamp')
-    except Exception as e:
-        print(e)
-        print(f"for either {ticker_buy} or {ticker_sell}")
-        return ticker_buy
-
-    ## least active first
-    volume1 = history1['amount'].sum()
-    volume2 = history2['amount'].sum()
-
-    result = ticker_buy if volume1<volume2 else ticker_sell
-    return result
-
-# size >0, in coin
-@timer
-async def execute_spread(exchange: Exchange, ticker_buy: str, ticker_sell: str, size_buy: float,size_sell: float) -> None:
-    if most_illiquid(exchange, ticker_buy, ticker_sell)==ticker_buy:
-        (first_ticker,second_ticker,first_side,second_side,first_size,second_size) = \
-            (ticker_buy,ticker_sell,
-            'buy','sell',
-             size_buy,size_sell)
-    else:
-        (first_ticker, second_ticker, first_side, second_side,first_size,second_size) = \
-            (ticker_sell,ticker_buy,
-             'sell','buy',
-             size_sell,size_buy)
-
-    try:
-        order = await sure_postAggressive(exchange, first_ticker, first_side, first_size)
-        if order:
-            await chase(exchange,order,taker_trigger=999) # no trigger for first leg
-
-        order = await sure_postAggressive(exchange, second_ticker, second_side, second_size)
-        if order:
-            await chase(exchange,order,taker_trigger=taker_trigger)
-    except Exception as e:
-        print(e)
-
-    return None
-
-# size >0, in coin
-@timer
-async def execute_single(exchange, ticker, size):
-    order = await sure_postAggressive(exchange, ticker, 'buy' if size > 0 else 'sell',np.abs(size))
-    if order:
-        await chase(exchange, order, taker_trigger=taker_trigger)
+#################### executers:  ##############################
 
 # size are + or -, in USD
 async def slice_spread_order(exchange: Exchange, ticker1: str, ticker2: str, size1: float, size2: float)->None:
@@ -228,21 +212,27 @@ async def slice_spread_order(exchange: Exchange, ticker1: str, ticker2: str, siz
         return None
 
     # slice the spread
-    (spread_ticker_buy, spread_ticker_sell) = (ticker1, ticker2) if size1 > 0 else (ticker2, ticker1)
     spread_size = min(np.abs(size1),np.abs(size2))
     amount_sliced = 0
     execution_report = pd.DataFrame()
     while amount_sliced + 2 * slice_size < spread_size:
         price1 = float(exchange.fetch_ticker(ticker1)['info']['price'])
         price2 = float(exchange.fetch_ticker(ticker2)['info']['price'])
-        await execute_spread(exchange, spread_ticker_buy, spread_ticker_sell,
-                             slice_size/price1, slice_size/price2)
+        if most_illiquid(exchange, ticker1, ticker2) == ticker1:
+            await post_chase_trigger(exchange, ticker1, np.sign(size1) * slice_size / price1, taker_trigger=999)
+            await post_chase_trigger(exchange, ticker2, np.sign(size2) * slice_size / price2, taker_trigger=taker_trigger)
+        else:
+            await post_chase_trigger(exchange, ticker2, np.sign(size2) * slice_size / price2, taker_trigger=999)
+            await post_chase_trigger(exchange, ticker1, np.sign(size1) * slice_size / price1, taker_trigger=taker_trigger)
         amount_sliced += slice_size
     print(f'spread {ticker1}/{ticker2} done in {amount_sliced}')
+
     if ((spread_size-amount_sliced)/price1>increment1)&((spread_size-amount_sliced)/price2>increment2):
-        await execute_spread(exchange, spread_ticker_buy, spread_ticker_sell,
-                         (spread_size-amount_sliced)/price1,(spread_size-amount_sliced)/price2)
-    print(f'spread residual {ticker1}/{ticker2} done in {spread_size-amount_sliced}')
+        print('residual:')
+        await slice_spread_order( exchange,
+                            ticker1,ticker2,
+                            np.sign(size1)*(spread_size-amount_sliced)/price1,
+                            np.sign(size2) * (spread_size - amount_sliced) / price2)
 
     # residual, from book
     diff=diff_portoflio(exchange)
@@ -266,7 +256,7 @@ async def slice_single_order(exchange: Exchange, ticker: str, size: float)->None
     amount_sliced = 0
     while amount_sliced + 2 * slice_size < np.abs(size):
         price=float(exchange.fetch_ticker(ticker)['info']['price'])
-        await execute_single(exchange, ticker, np.sign(size)*slice_size/price)
+        await post_chase_trigger(exchange, ticker, np.sign(size)*slice_size/price, taker_trigger=taker_trigger)
         amount_sliced += slice_size
     print(f'single {ticker} done in {np.sign(size)*amount_sliced}')
 
@@ -309,25 +299,31 @@ async def execute_weights(exchange,weights):
         #if leftover_delta>slice_sizeUSD/slice_factor:
         #    exchange.createOrder(future, 'market', order_status['side'], order_status['remaining'])
 
-if True:
-    exchange = open_exchange('ftx', 'SysPerp')
-    weights = diff_portoflio(exchange, 'Runtime/ApprovedRuns/current_weights.xlsx')
-    weights = weights[weights['diff'].apply(np.abs) > slice_factor]
-    print(weights)
-#    weights = weights[weights['name'].isin(['LINK/USD','LINK-PERP'])]
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.argv.extend(['execute'])
+    if len(sys.argv) < 5:
+        sys.argv.extend(['ftx', 'SysPerp', 'Runtime/ApprovedRuns/current_weights.xlsx'])
+        print(f'using defaults {sys.argv[2]} {sys.argv[3]} {sys.argv[4]}')
+    if sys.argv[1] == 'execute':
+        exchange = open_exchange(sys.argv[2], sys.argv[3])
+        weights = diff_portoflio(exchange, sys.argv[4])
+        weights = weights[weights['diff'].apply(np.abs) > slice_factor]
+        print(weights)
+    #    weights = weights[weights['name'].isin(['LINK/USD','LINK-PERP'])]
 
-    start_time = datetime.now().timestamp()
-    try:
-        asyncio.run(execute_weights(exchange,weights))
-        #asyncio.run(clean_dust(exchange))
-    except Exception as e:
-        print(e)
-    finally:
-        exchange.cancel_all_orders()
-        end_time = datetime.now().timestamp()
-        with pd.ExcelWriter('Runtime/execution_diagnosis.xlsx', engine='xlsxwriter') as writer:
-            pd.DataFrame(exchange.fetch_orders(params={'start_time':start_time,'end_time':end_time})).to_excel(writer,sheet_name='fills')
-            pd.DataFrame(audit).to_excel(writer,sheet_name='audit')
+        start_time = datetime.now().timestamp()
+        try:
+            asyncio.run(execute_weights(exchange,weights))
+            #asyncio.run(clean_dust(exchange))
+        except Exception as e:
+            print(e)
+        finally:
+            exchange.cancel_all_orders()
+            end_time = datetime.now().timestamp()
+            with pd.ExcelWriter('Runtime/execution_diagnosis.xlsx', engine='xlsxwriter') as writer:
+                pd.DataFrame(exchange.fetch_orders(params={'start_time':start_time,'end_time':end_time})).to_excel(writer,sheet_name='fills')
+                pd.DataFrame(audit).to_excel(writer,sheet_name='audit')
 
-        stats = fetch_latencyStats(exchange,days=1,subaccount_nickname='SysPerp')
-        print(stats)
+            stats = fetch_latencyStats(exchange,days=1,subaccount_nickname='SysPerp')
+            print(stats)
