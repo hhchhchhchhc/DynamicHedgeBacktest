3486c0dab47d909aa96824f7ea66467fac536ea1
daviidarr <davidrelkin@gmail.com>
2022-01-20 13:10:53 +0000

    fix

--- DerivativeArbitrage/Mess/asynctest.py
+++ DerivativeArbitrage/Mess/asynctest.py
@@ -1,7 +1,7 @@
 import time
 import ccxtpro
 import asyncio
-from ftx_utilities import open_exchange
+from ftx_utilities import *
 
 class myFtx(ccxtpro.ftx):
     def __init__(self,symbol: str,depth: float,config={}):
@@ -13,36 +13,39 @@ class myFtx(ccxtpro.ftx):
         message = super().handle_my_trade(client, message)
         return None
 
-
-
     def handle_order_book_update(self, client, message):
         super().handle_order_book_update(client, message)
         if message['market']==self._symbol:
             print('')
 
-async def place_limit_at_depth(self,symbol):
+async def place_limit_at_depth(self,symbol,tolerance=0.001):
     depth = 0
     for pair in self.orderbooks[symbol]['bids']:
         depth += pair[0] * pair[1]
         if depth > 10000:
             break
-    if self.orders and (symbol in self.orders.keys()):
-        order = await self.edit_order(self.orders[symbol][0]['id'], symbol, 'limit', 'buy', 10 / pair[0], pair[0],
-                        params={'postOnly': True})
+
+    if self.orders and symbol in self.orders.hashmap:
+        if len(self.orders.hashmap[symbol])>1:
+            print('?')
+        for item in self.orders.hashmap[symbol].items():
+            if (item[1]['status'] == 'open')&(np.abs(item[1]['price']/pair[0]-1)>tolerance):
+                order = await self.edit_order(item[0], symbol, 'limit', 'buy', 10 / pair[0],
+                                          pair[0],
+                                          params={'postOnly': True})
     else:
         order = await self.create_limit_order(symbol, 'buy', 10 / pair[0], pair[0], params={'postOnly': True})
-    return order
+    return None
 
-async def order_book_loop(exchange,symbol):
+async def watch_order_book(exchange,symbol):
     while True:
         await asyncio.sleep(1)
-#        order_book = await exchange.watch_order_book(symbol)
-#        order = await place_limit_at_depth(exchange,symbol)#.create_limit_order(symbol, 'buy', 1, 0.00001, params={'postOnly': True})
+        order_book = await exchange.watch_order_book(symbol)
+        order = await place_limit_at_depth(exchange,symbol)#.create_limit_order(symbol, 'buy', 1, 0.00001, params={'postOnly': True})
 
-async def order_loop(exchange,symbol):
+async def watch_orders(exchange,symbol):
     while True:
         try:
-            await asyncio.sleep(1)
             orders = await exchange.watch_orders(symbol)
             print('<---------')
             #print(orders)
@@ -50,29 +53,29 @@ async def order_loop(exchange,symbol):
             print(e)
 
 async def main(loop):
-    dummy=open_exchange('ftx','debug')
-    await dummy.close()
-
-    symbol = 'LEO/USD'
-    exchange=ccxtpro.ftx(config={
-        'asyncio_loop': loop,
-        'newUpdates':True,
+    symbol = 'GRT/USD'
+    exchange=ccxtpro.ftx({
+        #'asyncio_loop': loop,
+        'newUpdates':False,
         'enableRateLimit': True,
-        'apiKey': dummy.apiKey,
-        'secret': dummy.secret})
+        'apiKey': min_iterations,
+        'secret': max_iterations})
     exchange.verbose = True
-    exchange.headers = dummy.headers
+    exchange.headers= {'FTX-SUBACCOUNT': 'debug'}
 
     exchange.authenticate()
     await exchange.load_markets()
 
-    try:
-        await asyncio.gather(*[order_book_loop(exchange,symbol),
-                                   order_loop(exchange,symbol)])
-    except Exception as e:
-        print(type(e).__name__, str(e))
-        await exchange.cancel_all_orders()
-        await exchange.close()
+    while True:
+        try:
+            await asyncio.gather(*[watch_order_book(exchange,symbol),
+                                       watch_orders(exchange,symbol)])
+        except Exception as e:
+            print(type(e).__name__, str(e))
+
+    await exchange.cancel_all_orders()
+    await exchange.close()
+
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
