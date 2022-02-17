5548e342011fd8f0ac476e85ab7558251606ae2d
daviidarr <davidrelkin@gmail.com>
2022-02-17 12:37:32 +0000

    strat/exec/plex loop

---
+++ DerivativeArbitrage/main_loop.py
@@ -0,0 +1,41 @@
+from ftx_utilities import *
+from ftx_portfolio import ftx_portoflio_main
+from strategies import strategies_main
+from ftx_ws_execute import ftx_ws_spread_main
+from time import sleep
+from datetime import *
+
+launchtime_minutes = [36]
+throttle_minutes = 1
+
+while True:
+    minutes_to_sleep = min([t-datetime.now().minute for t in launchtime_minutes])
+    if minutes_to_sleep<0: minutes_to_sleep+=60
+
+    print(f'awaiting refresh time for {minutes_to_sleep} mins')
+    sleep(60*max(0,minutes_to_sleep))
+    while True:
+        try:
+            strategies_main('sysperp')
+        except Exception as e:
+            print(f'restarting strategy after {throttle_minutes} mins. Failed with {str(e)}')
+            sleep(throttle_minutes * 60)
+            continue
+        else:
+            break
+    print(f'weights refreshed. {throttle_minutes} mins before exec ')
+    sleep(throttle_minutes * 60)
+
+    while True:
+        try:
+            ftx_ws_spread_main('sysperp')
+        except Exception as e:
+            print(f'restarting exec after {throttle_minutes} mins. Failed with {str(e)}')
+            sleep(throttle_minutes * 60)
+            continue
+        else:
+            print(f'weights executed. Running plex')
+            ftx_portoflio_main('plex')
+            print(f'plex ran')
+            break
+    sleep(throttle_minutes * 60)
