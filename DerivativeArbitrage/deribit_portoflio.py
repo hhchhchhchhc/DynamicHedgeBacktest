38be78446f270771493c5a3ceb9b81366a6f89bd
daviidarr <davidrelkin@gmail.com>
2022-03-27 22:22:51 +0100

    minor changes

--- DerivativeArbitrage/deribit_portoflio.py
+++ DerivativeArbitrage/deribit_portoflio.py
@@ -63,39 +63,49 @@ slippage = {'delta':0, # 1 means 1%
             'rho':0}
 
 class Instrument:
-    '''everything is BTC margined so beware confusions:
+    '''everything is coin margined so beware confusions:
     self.notional is in USD
-    self.mark and all greeks are in BTC
-    +ve Delta still means long BTC, though, and is in USD'''
+    self.mark and all greeks are in coin
+    +ve Delta still means long coin, though, and is in USD'''
     def process_margin_call(self, market):
         previous_mark = self.mark
         self.mark = self.pv(market)
         return self.mark - previous_mark
 
+class Cash(Instrument):
+    def __init__(self,currency):
+        self.mark = 1
+        self.symbol = currency
+    def pv(self, market):
+        return 1
+    def process_cash_flow(self, market):
+        return 0
+
 class InverseFuture(Instrument):
-    '''a long perpetual (size in USD, strike in BTCUSD) is a short USDBTC(notional in USD, 1/strike in USDBTC
+    '''a long perpetual (size in USD, strike in coinUSD) is a short USDcoin(notional in USD, 1/strike in USDcoin
     what we implement is more like a fwd..'''
     greeks_available = ['delta','theta']
-    def __init__(self, strike_BTCUSD, maturity, market):
-        self.strike = 1 / strike_BTCUSD
+    def __init__(self, underlying, strike_coinUSD, maturity, market):
+        self.strike = 1 / strike_coinUSD
         self.maturity = maturity
         self.mark = self.pv(market)
-        self.symbol = 'BTC-1m'# + str(pd.to_datetime(maturity, unit='s'))
+        self.symbol = underlying+'-1m'# + str(pd.to_datetime(maturity, unit='s'))
 
     def pv(self, market):
-        '''for a 1% move'''
         T = (self.maturity - market['t'])
         df = np.exp(-market['r']*T)
-        return df * (self.strike - 1 / market['fwd']) / market['fwd']
+        return df * (self.strike - 1 / market['fwd'])
     def delta(self, market):
+        '''for a 1% move'''
         T = (self.maturity - market['t'])
-        return np.exp(-market['r']*T) * 0.01
+        return np.exp(-market['r']*T) * 0.01 / market['fwd']
     def theta(self, market):
+        '''for 1 day'''
         T = (self.maturity - market['t'])
         df = np.exp(-market['r'] * T)
         return - market['r'] * df / 24/365.25
     def process_cash_flow(self, market):
-        '''cash settled in BTC'''
+        '''cash settled in coin'''
         if market['prev_t'] < self.maturity and market['t'] >= self.maturity:
             cash_flow = (1 / market['fwd'] - self.strike)
             self.mark = 0
@@ -107,19 +117,19 @@ class InverseFuture(Instrument):
         return -6e-3
 
 class InversePerpetual(Instrument):
-    '''a long perpetual (size in USD, strike in BTCUSD) is a short USDBTC(notional in USD, 1/strike in USDBTC
+    '''a long perpetual (size in USD, strike in coinUSD) is a short USDcoin(notional in USD, 1/strike in USDcoin
     what we implement is more like a fwd..'''
     greeks_available = ['delta']
-    def __init__(self, strike_BTCUSD, market):
-        self.strike = 1 / strike_BTCUSD
+    def __init__(self, underlying, strike_coinUSD, market):
+        self.strike = 1 / strike_coinUSD
         self.mark = self.pv(market)
-        self.symbol = 'BTC-PERPETUAL'
+        self.symbol = underlying+'-PERPETUAL'
 
     def pv(self, market):
-        '''for a 1% move'''
-        return (self.strike - 1 / market['fwd']) / market['fwd']
+        return (self.strike - 1 / market['fwd'])
     def delta(self, market):
-        return 0.01
+        '''for a 1% move'''
+        return 0.01 / market['fwd']
     def process_cash_flow(self, market):
         '''accrues every millisecond.
         8h Funding Rate = Maximum (0.05%, Premium Rate) + Minimum (-0.05%, Premium Rate)
@@ -127,14 +137,14 @@ class InversePerpetual(Instrument):
         return - market['fundingRate'] * (market['t'] - market['prev_t'])/3600/8 / market['spot']
     def margin_value(self, market):
         '''TODO: approx, in fact it's converted at spot not fwd'''
-        return -6e-3
+        return -6e-3 / market['spot']
 
 class Option(Instrument):
-    '''a BTCUSD call(size in btc, strike in BTCUSD) is a USDBTC put(strike*size in USD,1/strike in USDTBC)
-    notional in USD, strike in BTC, maturity in sec, callput = C or P '''
+    '''a coinUSD call(size in coin, strike in coinUSD) is a USDcoin put(strike*size in USD,1/strike in USDTBC)
+    notional in USD, strike in coin, maturity in sec, callput = C or P '''
     greeks_available = ['delta', 'gamma', 'vega', 'theta']
-    def __init__(self, strike_BTCUSD, maturity, call_put,market):
-        self.strike = 1 / strike_BTCUSD
+    def __init__(self, underlying,strike_coinUSD, maturity, call_put,market):
+        self.strike = 1 / strike_coinUSD
         self.maturity = maturity
         if call_put == 'C':
             self.call_put = 'P'
@@ -144,7 +154,7 @@ class Option(Instrument):
             self.call_put = 'S'
         else: raise Exception("unknown option type")
         self.mark = self.pv(market)
-        self.symbol = 'BTC-' + call_put + '-1m'# + str(pd.to_datetime(maturity, unit='s')) + '+' + str(int(strike_BTCUSD))
+        self.symbol = underlying+'-' + call_put + '-1m'# + str(pd.to_datetime(maturity, unit='s')) + '+' + str(int(strike_coinUSD))
 
     def pv(self, market):
         T = (self.maturity - market['t'])
@@ -174,7 +184,7 @@ class Option(Instrument):
         return theta_fwd - market['r'] * df / 24/365.25
 
     def process_cash_flow(self, market):
-        '''cash settled in BTC'''
+        '''cash settled in coin'''
         if market['prev_t'] < self.maturity and market['t'] >= self.maturity:
             cash_flow = max(0,(1 if self.call_put == 'C' else -1)*(1 / market['fwd'] - self.strike))
             self.mark = 0
@@ -193,17 +203,14 @@ class Position:
         return self.notional * getattr(self.instrument, greek)(market) if hasattr(self.instrument,greek) else 0
         
 class Portfolio():
-    def __init__(self,notional_BTC):
-        self.cash = notional_BTC
-        self.positions = []
+    def __init__(self,currency, notional_coin):
+        self.positions = [Position(Cash(currency),notional_coin)]
 
     def apply(self, greek, market):
         result = sum([position.apply(greek,market) for position in self.positions])
-        if greek == 'pv':
-            result += self.cash
         return result
 
-    def new_trade(self, instrument, notional_USD, price_BTC = 'slippage', market = None):
+    def new_trade(self, instrument, notional_USD, price_coin = 'slippage', market = None):
         position = next(iter(x for x in self.positions if x.instrument == instrument), 'new_position')
         if position == 'new_position':
             position = Position(instrument,notional_USD)
@@ -214,15 +221,15 @@ class Portfolio():
             position.notional += notional_USD
 
         # slippage
-        if price_BTC == 'slippage':
+        if price_coin == 'slippage':
             assert market is not None
             mid = instrument.pv(market)
             slippage_cost = sum(
                 slippage[greek] * np.abs(self.apply(greek,market)) for greek in instrument.__class__.greeks_available)
-            price_BTC = mid + slippage_cost * np.sign(notional_USD)
+            price_coin = mid + slippage_cost * np.sign(notional_USD)
             
         # pay premium. will be mopped up by next margin call
-        self.cash -= notional_USD * price_BTC
+        self.positions[0].notional -= notional_USD * price_coin
 
     def delta_hedge(self, hedge_instrument, market):
         portfolio_delta = self.apply('delta',market)
@@ -231,13 +238,15 @@ class Portfolio():
 
         self.new_trade(hedge_instrument,
                        hedge_notional,
-                       price_BTC= 'slippage',
+                       price_coin= 'slippage',
                        market = market)
 
     def gamma_target(self, target, market, hedge_instrument = 'atm', hedge_mode = 'add'):
         '''hedge_instrument in ['atm,an Option]'''
         if hedge_instrument == 'atm':
-            hedge_instrument = Option(strike_BTCUSD=market['fwd'],
+            hedge_instrument = Option(
+                underlying=self.positions[0].instrument.symbol,
+                        strike_coinUSD=market['fwd'],
                       maturity=market['t'] + 3600 * 24 * 30,
                       call_put='S',
                       market=market)
@@ -247,7 +256,7 @@ class Portfolio():
         if hedge_mode == 'replace':
             [self.new_trade(position.instrument,
                             -position.notional,
-                            price_BTC= 'slippage',
+                            price_coin= 'slippage',
                             market = market
                             ) for position in self.positions if isinstance(position.instrument,Option)]
         portfolio_gamma = self.apply('gamma', market)
@@ -256,47 +265,63 @@ class Portfolio():
 
         self.new_trade(hedge_instrument,
                        hedge_notional,
-                       price_BTC= 'slippage',
+                       price_coin= 'slippage',
                        market = market)
 
     def process_cash_flows(self, market):
-        '''cash_flows before margin calls !!'''
+        '''cash_flows must be called before margin calls !!'''
         cash_flows = sum([position.notional * getattr(position.instrument, 'process_cash_flow')(market) 
                           for position in self.positions])
         margin_call = sum([position.notional * getattr(position.instrument, 'process_margin_call')(market) 
                            for position in self.positions])
-        self.cash += cash_flows + margin_call
+        self.positions[0].notional += cash_flows + margin_call
 
-def display_current(portfolio,market,prev_market):
-    predictor = {'pv':lambda greek: greek - portfolio.apply(greek='pv', market=prev_market),# this is actual pnl in fact
-                 'delta':lambda greek: greek*(market['fwd']/prev_market['fwd']-1)*100,
-                 'gamma':lambda greek: greek*(market['fwd']/prev_market['fwd']-1)**2 *100*100,
+        for position in self.positions:
+            if hasattr(position.instrument, 'maturity') and market['t'] > position.instrument.maturity:
+                self.positions.remove(position)
+
+def display_current(portfolio,prev_portfolio, market,prev_market):
+    predictors = {'pv':lambda greek: portfolio.apply(greek='pv', market=market) - portfolio.apply(greek='pv', market=prev_market),
+                 'delta':lambda greek: greek*(prev_market['fwd']/market['fwd']-1)*100,
+                 'gamma':lambda greek: 0.5 * greek*(prev_market['fwd']/market['fwd']-1)**2 *100*100,
                  'vega':lambda greek: greek*(market['vol']/prev_market['vol']-1)*10,
                  'theta':lambda greek: greek*(market['t']-prev_market['t'])/3600}
 
     display_market = pd.Series({('mkt',data,'total'):market[data] for data in ['prev_t', 't', 'spot', 'fwd', 'dF/F', 'vol', 'fundingRate', 'borrow', 'r']})
     total_greeks = pd.Series(
-        {('risk',greek,'total'): portfolio.apply(greek=greek, market=market)
-                   for greek in predictor.keys()})
+        {('risk',greek,'total'):
+             portfolio.apply(greek=greek, market=market)
+         for greek in predictors.keys()})
     greeks_per_instrument = pd.Series(
-        {('risk',greek,position.instrument.symbol): position.apply(greek, market)
-                   for greek in predictor.keys() for (i, position) in enumerate(portoflio.positions)}
-                  | {('risk','pv','cash'): portoflio.cash})
+        {('risk',greek,position.instrument.symbol):
+             position.apply(greek, market)
+         for greek in predictors.keys() for position in portfolio.positions})
     total_predict = pd.Series(
-        {('predict',greek,'total'): predictor(portfolio.apply(greek=greek, market=market))
-                   for greek,predictor in predictor.items()})
+        {('predict',greek,'total'):
+             predictor(portfolio.apply(greek=greek, market=market))
+         for greek,predictor in predictors.items()}
+        |{('predict', 'new_deal', 'total'):
+              portfolio.apply(greek='pv',market=market) - prev_portfolio.apply(greek='pv', market=market)})
     predict_per_instrument = pd.Series(
-        {('predict',greek,position.instrument.symbol): predictor(position.apply(greek, market))
-                   for greek,predictor in predictor.items() for (i, position) in enumerate(portoflio.positions)})
+        {('predict',greek,position.instrument.symbol):
+             predictor(position.apply(greek, market))
+         for greek,predictor in predictors.items() for position in portfolio.positions}
+        |{('predict', 'new_deal', position.instrument.symbol):
+              portfolio.apply(greek='pv',market=market) - prev_portfolio.apply(greek='pv', market=market)
+          for position in portfolio.positions})
 
     new_data = pd.concat([display_market,total_greeks,total_predict,greeks_per_instrument,predict_per_instrument],axis=0)
     new_data.name = pd.to_datetime(market['t'], unit='s')
     return new_data
 
 if __name__ == "__main__":
+    if len(sys.argv)<2:
+        currency = 'BTC'
+    else:
+        currency = sys.argv[1]
 
     ## get history
-    history = deribit_history_main('just use',['BTC'],'deribit','cache')[0]
+    history = deribit_history_main('just use',[currency],'deribit','cache')[0]
 
     # 1mFwd
     if False: # not enough history unless we fetch all expired futures
@@ -307,15 +332,15 @@ if __name__ == "__main__":
             x=np.array([t[tenor] for tenor in tenor_columns if not pd.isna(t[tenor])]),
             y=np.array([t[rate] for rate in mark_columns if not pd.isna(t[rate])]),
             kind='linear', fill_value='extrapolate')(1/12), axis=1)
-    history['1mFwd'] = history['BTC-PERPETUAL/mark/c']
+    history['1mFwd'] = history[currency+'-PERPETUAL/mark/c']
 
     ## format history
     history.reset_index(inplace=True)
     history.rename(columns={'index':'t',#TODO: more consistent with /o
                             '1mFwd':'fwd',
-                            'BTC/volindex/c':'vol',
-                            'BTC-PERPETUAL/indexes/c':'spot',
-                            'BTC-PERPETUAL/rate/funding':'fundingRate'},inplace=True)
+                            currency+'/volindex/c':'vol',
+                            currency+'-PERPETUAL/indexes/c':'spot',
+                            currency+'-PERPETUAL/rate/funding':'fundingRate'},inplace=True)
     history['dF/F'] = history['fwd'].apply(lambda f:np.log(f)).diff()
     history['t'] = history['t'].apply(lambda t:t.timestamp())
     history['prev_t'] = history['t'].shift(1)
@@ -323,40 +348,51 @@ if __name__ == "__main__":
     history['borrow'] = 0
     history['r'] = history['borrow']
     history.ffill(limit=2,inplace=True)
+    history.bfill(inplace=True)
+
+    prev_mkt = history.iloc[0]
 
     ## new portfolio
-    portoflio = Portfolio(notional_BTC=1)
-
-    # at time 0 sell a straddle
-    prev_mkt = history.iloc[0]
-    hedge_instrument = InversePerpetual(strike_BTCUSD=prev_mkt['fwd'],market=prev_mkt)
-    straddle = Option(strike_BTCUSD=prev_mkt['fwd'],
+    portfolio = Portfolio(currency,notional_coin=1)
+    hedge_instrument = InversePerpetual(underlying=currency,
+                                        strike_coinUSD=prev_mkt['fwd'],
+                                        market=prev_mkt)
+    portfolio.new_trade(instrument= hedge_instrument,
+                        notional_USD= -prev_mkt['fwd'],
+                        price_coin= 'slippage',
+                        market= prev_mkt)
+    straddle = Option(underlying=currency,
+                      strike_coinUSD=prev_mkt['fwd'],
                       maturity=prev_mkt['t'] + 3600 * 24 * 30,
                       call_put='S',
                       market=prev_mkt)
-    portoflio.new_trade(instrument= straddle,
-                        notional_USD= -prev_mkt['fwd'],
-                        price_BTC= 'slippage',
+    portfolio.new_trade(instrument= straddle,
+                        notional_USD= -prev_mkt['fwd']*0,
+                        price_coin= 'slippage',
                         market= prev_mkt)
+    target_gamma = portfolio.apply('gamma',prev_mkt)
+    prev_portfolio = portfolio
 
     ## run backtest
     series = []
     for _,market in history.iterrows():
-        portoflio.process_cash_flows(market)
-        portoflio.gamma_target(portoflio.apply('gamma',market),market,hedge_instrument='atm',hedge_mode='replace')
-        portoflio.delta_hedge(hedge_instrument,market)
+        portfolio.process_cash_flows(market)
+        #portfolio.gamma_target(target_gamma,market,hedge_instrument='atm',hedge_mode='replace')
+        #portfolio.delta_hedge(hedge_instrument,market)
         
-        series += [display_current(portoflio,market,prev_mkt)]
+        series += [display_current(portfolio,prev_portfolio,market,prev_mkt)]
         prev_mkt = market
+        prev_portfolio = portfolio
 
     display = pd.concat(series,axis=1)
     display.sort_index(axis=0,level=0).T.to_excel('Runtime/runs/deribit.xlsx')
+    display.sort_index(axis=0, level=0).to_pickle('Runtime/runs/deribit.pickle')
 
 if False:
-    btc = pd.read_csv("C:/Users/david/pyCharmProjects/SystematicCeFi/DerivativeArbitrage/Runtime/Deribit_Mktdata_database/deribit_options_chain_2019-07-01_BTC.csv")
-    btc['expiry'] = btc['expiration'].apply(lambda t: pd.to_datetime(int(t), unit='us'))
-    btc['timestamp'] = btc['timestamp'].apply(lambda t: pd.to_datetime(int(t), unit='us'))
-    btc['dist_2_fwd'] = btc.apply(lambda f: np.abs(np.log(f['underlying_price']/f['strike_price'])),axis=1)
-    smiles = btc.groupby(by=['expiry', 'timestamp'])[['timestamp','delta','mark_iv']]
+    coin = pd.read_csv("C:/Users/david/pyCharmProjects/SystematicCeFi/DerivativeArbitrage/Runtime/Deribit_Mktdata_database/deribit_options_chain_2019-07-01_coin.csv")
+    coin['expiry'] = coin['expiration'].apply(lambda t: pd.to_datetime(int(t), unit='us'))
+    coin['timestamp'] = coin['timestamp'].apply(lambda t: pd.to_datetime(int(t), unit='us'))
+    coin['dist_2_fwd'] = coin.apply(lambda f: np.abs(np.log(f['underlying_price']/f['strike_price'])),axis=1)
+    smiles = coin.groupby(by=['expiry', 'timestamp'])[['timestamp','delta','mark_iv']]
     #.sort_values(by='strike_price',
                                                              #key=lambda f: np.abs(f['underlying_price'] / f['strike']))[-1]
