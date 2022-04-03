678de450263842a193225178df7cdb52074ee046
daviidarr <davidrelkin@gmail.com>
2022-04-03 11:59:20 +0100

    minor

--- DerivativeArbitrage/windows_read_logs.bat
+++ DerivativeArbitrage/windows_read_logs.bat
@@ -1,2 +1,7 @@
-python .\ftx_utilities.py
-start excel ".\Runtime\logs\latest_exec.xlsx"
+taskkill /F /IM excel.exe
+python .\deribit_portoflio.py
+start excel ".\Runtime\runs\deribit.xlsx
+
+
+::python .\side_scripts.py ::"log_reader"
+::start excel ".\Runtime\logs\latest_exec.xlsx"
