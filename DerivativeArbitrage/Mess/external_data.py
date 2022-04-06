945a290847566a4126c95a2ebd78a0d3dfeb8920
daviidarr <davidrelkin@gmail.com>
2022-04-06 13:00:54 +0100

    ribbon auctions

--- DerivativeArbitrage/Mess/external_data.py
+++ DerivativeArbitrage/Mess/external_data.py
@@ -1,30 +1,39 @@
 import csv,sys
-import requests,asyncio
+import requests,asyncio,functools
 import pandas as pd
 from datetime import *
 from tardis_client import TardisClient, Channel
 from tardis_dev import datasets, get_exchange_details
 
-def get_data_feeds(start, end, exchange = 'deribit'):
-    exchange_details = get_exchange_details(exchange)
+def async_wrap(f):
+    @functools.wraps(f)
+    async def run(*args, loop=None, executor=None, **kwargs):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+        p = functools.partial(f, *args, **kwargs)
+        return await loop.run_in_executor(executor, p)
+    return run
 
-    datasets.download(
+def get_data_feeds(start, end, symbols = 'OPTION_CHAIN', exchange = 'deribit'):
+    #exchange_details = get_exchange_details(exchange)
+
+    # datasets.download(
+    #     exchange=exchange,
+    #     data_types=['derivative_ticker'],
+    #     from_date=str(start),
+    #     to_date=str(end),
+    #     symbols=['PERPETUALS','FUTURES'],
+    #     api_key="TD.5sHAB2mPdsHYylP6.Y9KLWdd3XIeOpgv.7KnfWYukxDDuyFu.WUi-a3IvCvWnavo.iekVlRGaldwInlo.QJSZ",
+    #     # path where CSV data will be downloaded into
+    #     download_dir="Runtime/Tardis"
+    # )
+
+    return datasets.download(
         exchange=exchange,
-        data_types=['derivative_ticker'],
+        data_types=['trades','book_snapshot_5'],
         from_date=str(start),
         to_date=str(end),
-        symbols=['PERPETUALS','FUTURES'],
-        api_key="TD.5sHAB2mPdsHYylP6.Y9KLWdd3XIeOpgv.7KnfWYukxDDuyFu.WUi-a3IvCvWnavo.iekVlRGaldwInlo.QJSZ",
-        # path where CSV data will be downloaded into
-        download_dir="Runtime/Tardis"
-    )
-
-    datasets.download(
-        exchange=exchange,
-        data_types=['options_chain'],
-        from_date=str(start),
-        to_date=str(end),
-        symbols=['OPTIONS'],
+        symbols=symbols,
         api_key="TD.5sHAB2mPdsHYylP6.Y9KLWdd3XIeOpgv.7KnfWYukxDDuyFu.WUi-a3IvCvWnavo.iekVlRGaldwInlo.QJSZ",
         # path where CSV data will be downloaded into
         download_dir="Runtime/Tardis"
@@ -48,6 +57,40 @@ async def save_historical_deribit_index_data_to_csv():
 
     print("finished")
 
+ribbon_auctions = [
+    {'trade_date': datetime(2022,4,15,10)-timedelta(days=7),
+     'expiry_date': datetime(2022,4,15,10),
+     'strike': 4000,
+     'volume': 38000},
+    {'trade_date': datetime(2022, 4, 1, 10),
+     'expiry_date': '8APR22',
+     'strike': 3700,  # 3700
+     'volume': 38000},
+    {'trade_date': datetime(2022, 3, 25, 10),
+     'expiry_date': '1APR22',
+     'strike': 3500,#3700
+     'volume': 38000},
+    {'trade_date': datetime(2022, 3, 18, 10),
+     'expiry_date': '25MAR22',
+     'strike': 3200,
+     'volume': 38000},
+    {'trade_date': datetime(2022, 3, 11, 10),
+     'expiry_date': '18MAR22',
+     'strike': 3000,
+     'volume': 38000},
+    {'trade_date': datetime(2022, 3, 4, 10),
+     'expiry_date': '11MAR22',
+     'strike': 3100,
+     'volume': 38000},
+    {'trade_date': datetime(2022, 2, 25, 10),
+     'expiry_date': '4MAR22',
+     'strike': 3000,
+     'volume': 38000},
+    {'trade_date': datetime(2022, 2, 18, 10),
+     'expiry_date': '25FEB22',
+     'strike': 3300,
+     'volume': 38000},
+]
 
 def tardis_main(*argv):
     argv = list(argv)
@@ -55,9 +98,11 @@ def tardis_main(*argv):
         argv.extend(['rest'])
 
     if argv[0] == 'rest':
-        days = [datetime(year=2022,month=3,day=1),datetime(year=2022,month=3,day=1)]
-        #return asyncio.gather(*[get_data_feeds(start,end) for start,end in list(zip(days[:-1],days[1:]))])
-        return get_data_feeds(days[0],days[1])
+        symbols = ['ETH-'+auction['expiry_date']+'-'+str(auction['strike'])+'-C' for auction in ribbon_auctions[1:]]
+        coro = [async_wrap(get_data_feeds(auction['trade_date']-timedelta(hours=6),auction['trade_date']+timedelta(hours=2),[symbol]))
+                for symbol,auction in zip(symbols,ribbon_auctions[1:])]
+        data = asyncio.gather(*coro)
+        return data
     elif argv[0] == 'ws':
         return asyncio.run(save_historical_deribit_index_data_to_csv())
     else:
