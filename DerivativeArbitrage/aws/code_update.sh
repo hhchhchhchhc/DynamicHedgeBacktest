ba27bae4cf86a1d598954b3cb4a7f36b8bce23dd
daviidarr <davidrelkin@gmail.com>
2022-04-21 12:18:43 +0100

    deployment script fix

--- DerivativeArbitrage/aws/code_update.sh
+++ DerivativeArbitrage/aws/code_update.sh
@@ -1,4 +1,4 @@
-cd /home/ec2-user/
+cd /home/ec2-user/SystematicCeFi
 git pull https://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@github.com/daviidarr/SystematicCeFi.git
 source ~/SystematicCeFi/env/bin/activate
-pip3 install -r SystematicCeFi/DerivativeArbitrage/requirements.txt
+pip3 install -r DerivativeArbitrage/requirements.txt
